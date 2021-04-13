a2c586ab9df44ef3b7a889f27f0f77d46606c45b
AkihiroYamaguchi4 <81012815+AkihiroYamaguchi4@users.noreply.github.com>
2021-04-13 15:31:24 +0900

    エラーメッセージの表示

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,6 @@ class ApplicationController < ActionController::Base
   end
   private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:family_name,:first_name,:family_name_reading,:birth_id,])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:family_name,:first_name,:family_name_reading,:first_name_reading,:birth_id])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-end
+    with_options presence: true do 
+         validates :nickname
+         validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: '全角ひらがな、全角カタカナ、漢字で入力して下さい' }
+         validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: '全角ひらがな、全角カタカナ、漢字で入力して下さい' }
+         validates :family_name_reading, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナで入力して下さい' }
+         validates :first_name_reading, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナで入力して下さい' }
+         validates :birth_id
+         PASSWORD_REGEX = /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]+\z/i.freeze
+         validates_format_of :password, with: PASSWORD_REGEX, message: 'パスワードには半角英字と半角数字の両方を含めて設定してください' 
+    end
+  end
