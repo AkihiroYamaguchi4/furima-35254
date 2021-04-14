81384438c05d535307c2d56bf053fe4e3b1e2980
AkihiroYamaguchi4 <81012815+AkihiroYamaguchi4@users.noreply.github.com>
2021-04-14 12:37:40 +0900

    出品データの保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,18 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
+  end
+  
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:product, :category_id, :information, :condition_id,:shipping_fee_id, :prefecture_id, :days_to_ship_id, :selling_price, :image).merge(user_id: current_user.id)
   end
 end
