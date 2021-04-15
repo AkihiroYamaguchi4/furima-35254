2a618a1b9957543c9b70edbe7a3f32d7c528a54f
AkihiroYamaguchi4 <81012815+AkihiroYamaguchi4@users.noreply.github.com>
2021-04-15 11:40:42 +0900

    itemモデルバリテーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,9 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category,:condition,:shipping_fee,:prefecture,:days_to_ship
-  validates :category_id,:condition_id,:shipping_fee_id,:prefecture_id,:days_to_ship_id, numericality: { other_than: 1 }
+       belongs_to :category,:condition,:shipping_fee,:prefecture,:days_to_ship
+       validates :category_id,:condition_id,:shipping_fee_id,:prefecture_id,:days_to_ship_id, numericality: { other_than: 1 }
+     with_options format:{ with: /\A[0-9]+\z/, message:'半角数字で入力して下さい' } do
+       validates :selling_price, numericality: { in: 300..9999999 }
+     end
 end
