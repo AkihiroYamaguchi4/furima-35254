6c2550c2eb8e40f931e31f3fca9138d3844246ec
AkihiroYamaguchi4 <81012815+AkihiroYamaguchi4@users.noreply.github.com>
2021-04-16 17:25:49 +0900

    itemモデル単体テスト修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,11 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category, :condition, :shipping_fee, :prefecture, :days_to_ship
+  belongs_to :category 
+  belongs_to :condition
+  belongs_to :shipping_fee
+  belongs_to :prefecture
+  belongs_to :days_to_ship
   validates :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :days_to_ship_id, numericality: { other_than: 1 }
   with_options presence: true do
     validates :product
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -13,96 +13,96 @@ RSpec.describe Item, type: :model do
     end
 
     context '出品ができない時' do
-      it '商品名が空だとと出品できないこと' do
+      it '商品名が空だと出品できないこと' do
         @item.product = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Product can't be blank")
       end
-      it '画像が空だとと出品できないこと' do
+      it '画像が空だと出品できないこと' do
         @item.image = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Image can't be blank")
       end
-      it '説明が空だとと出品できないこと' do
+      it '説明が空だと出品できないこと' do
         @item.information = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Information can't be blank")
       end
-      it 'カテゴリーが空だとと出品できないこと' do
+      it 'カテゴリーが空だと出品できないこと' do
         @item.category_id = ""
         @item.valid?
         expect(@item.errors.full_messages).to include("Category is not a number")
       end
-      it '状態が空だとと出品できないこと' do
+      it '状態が空だと出品できないこと' do
         @item.condition_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include('Condition is not a number')
       end
-      it '状態が---だとと出品できないこと' do
+      it '状態が---だと出品できないこと' do
         @item.condition_id = "1"
         @item.valid?
         expect(@item.errors.full_messages).to include('Condition must be other than 1')
       end
       
 
-      it '発送の負担が空だとと出品できないこと' do
+      it '発送の負担が空だと出品できないこと' do
         @item.shipping_fee_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include('Shipping fee is not a number')
       end
-      it '発送の負担が---だとと出品できないこと' do
-        @item.shipping_fee_id = '1'
+      it '発送の負担が---だと出品できないこと' do
+        @item.shipping_fee_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include('Shipping fee must be other than 1')
       end
-      it '発送地域が空だとと出品できないこと' do
+      it '発送地域が空だと出品できないこと' do
         @item.prefecture_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include('Prefecture is not a number')
       end
       it '発送地域が---だと出品できないこと' do
-        @item.prefecture_id = '1'
+        @item.prefecture_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include('Prefecture must be other than 1')
       end
-      it '発送の日数が空だとと出品できないこと' do
+      it '発送の日数が空だと出品できないこと' do
         @item.days_to_ship_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include('Days to ship is not a number')
       end
-      it '発送の日数が---だとと出品できないこと' do
-        @item.days_to_ship_id = '1'
+      it '発送の日数が---だと出品できないこと' do
+        @item.days_to_ship_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include('Days to ship must be other than 1')
       end
-      it '販売価格が空だとと出品できないこと' do
+      it '販売価格が空だと出品できないこと' do
         @item.selling_price = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Selling price can't be blank")
       end
-      it '販売価格が上限以上だとと出品できないこと' do
+      it '販売価格が上限以上だと出品できないこと' do
         @item.selling_price = 100000000
         @item.valid?
         expect(@item.errors.full_messages).to include("Selling price must be less than 9999999")
       end
-      it '販売価格が上限以下だとと出品できないこと' do
+      it '販売価格が上限以下だと出品できないこと' do
         @item.selling_price = 299
         @item.valid?
         expect(@item.errors.full_messages).to include('Selling price must be greater than 300')
       end
       
 
-      it '販売価格が全角数字だとと出品できないこと' do
+      it '販売価格が全角数字だと出品できないこと' do
         @item.selling_price = '２９９'
         @item.valid?
         expect(@item.errors.full_messages).to include('Selling price is not a number')
       end
-      it '販売価格が全角英語だとと出品できないこと' do
+      it '販売価格が全角英語だと出品できないこと' do
         @item.selling_price = 'TTT'
         @item.valid?
         expect(@item.errors.full_messages).to include('Selling price is not a number')
       end
-      it '販売価格が半角英数字を含む場合だとと出品できないこと' do
+      it '販売価格が半角英数字を含む場合だと出品できないこと' do
         @item.selling_price = '12a'
         @item.valid?
         expect(@item.errors.full_messages).to include('Selling price is not a number')
