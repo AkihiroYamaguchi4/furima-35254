8ce2fd9dc898bf9b492a2a7cc8ae787aead769e4
AkihiroYamaguchi4 <81012815+AkihiroYamaguchi4@users.noreply.github.com>
2021-04-19 20:56:49 +0900

    ロボコップ

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,9 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new,:create,:edit,:destroy]
-  before_action :set_item, only:[:show, :edit]
-  before_action :move_to_index, only: [:edit ]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :destroy]
+  before_action :set_item, only: [:show, :edit]
+  before_action :move_to_index, only: [:edit]
   def index
-    @items = Item.all.order("created_at DESC")
+    @items = Item.all.order('created_at DESC')
   end
 
   def new
@@ -22,38 +22,35 @@ class ItemsController < ApplicationController
   def show
   end
 
- def destroy
+  def destroy
     item = Item.find(params[:id])
-   if item.destroy
-    redirect_to action: :index
+    redirect_to action: :index if item.destroy
+  end
+
+  def edit
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
     end
- end
-
- def edit
- end
-
- def update
-  @item = Item.find(params[:id])
-  if @item.update(item_params)
-     redirect_to item_path
-  else 
-    render :edit
-   end
- end
+  end
 
   private
+
   def item_params
     params.require(:item).permit(:product, :category_id, :information, :condition_id, :shipping_fee_id, :prefecture_id,
                                  :days_to_ship_id, :selling_price, :image).merge(user_id: current_user.id)
   end
-  
+
   def set_item
     @item = Item.find(params[:id])
   end
 
   def move_to_index
-    unless user_signed_in? && current_user.id == @item.user_id
-      redirect_to action: :index
-    end
+    redirect_to action: :index unless user_signed_in? && current_user.id == @item.user_id
   end
 end
