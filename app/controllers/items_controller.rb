4a58b63e6cfc264bc498dc303d731d45ff5d3386
AkihiroYamaguchi4 <81012815+AkihiroYamaguchi4@users.noreply.github.com>
2021-04-19 17:04:09 +0900

    Revert "Merge pull request #6 from AkihiroYamaguchi4/削除機能の作成"
    This reverts commit a38ccfc7835777ac037aa5a6c888cf779ec5b838, reversing
    changes made to 2d84a5444a4e01defe50365349afd062066743e8.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,13 +21,6 @@ class ItemsController < ApplicationController
       @item = Item.find(params[:id])
   end
 
- def destroy
-    item = Item.find(params[:id])
-   if item.destroy
-    redirect_to action: :index
-    end
- end
-
   private
 
   def item_params
