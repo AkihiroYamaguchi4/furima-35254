17ae82b198a82637418070be4d314fe70bf905e1
AkihiroYamaguchi4 <81012815+AkihiroYamaguchi4@users.noreply.github.com>
2021-04-21 11:31:16 +0900

    クレジット機能サーバーサイド

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -6,13 +6,28 @@ class PurchasesController < ApplicationController
   end
 
  def create
-  binding.pry
   @puchase_shipping = PurchaseShippig.new(purchase_params)
   Shipping.create(shipping_params)
+  if @puchase_shipping.valid?
+    pay_item
+    @puchase_shipping.save
+    return redirect_to root_path
+  else
+    render 'index'
+  end
  end
 
  private
  def purchase_params
-  params.permit(:token).merge(user_id: current_user.id, item_id: params[:item_id])
+  params.require(:purchase).permit(:token).merge(token: params[:token],user_id: current_user.id, item_id: params[:item_id])
+ end
+
+ def pay_item
+  Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+    Payjp::Charge.create(
+      amount: order_params[:price],  # 商品の値段
+      card: order_params[:token],    # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+    )
  end
 end
