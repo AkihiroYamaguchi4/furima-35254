8ca0f1a9817e24654b170fc5ee6df8c81eea49ca
AkihiroYamaguchi4 <81012815+AkihiroYamaguchi4@users.noreply.github.com>
2021-04-21 11:13:21 +0900

    クレジット決済クライアントサイド

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,2 +1,18 @@
 class PurchasesController < ApplicationController
+  before_action :authenticate_user!
+  def index
+   @item = Item.new
+   @puchase_shipping = PurchaseShipping.new
+  end
+
+ def create
+  binding.pry
+  @puchase_shipping = PurchaseShippig.new(purchase_params)
+  Shipping.create(shipping_params)
+ end
+
+ private
+ def purchase_params
+  params.permit(:token).merge(user_id: current_user.id, item_id: params[:item_id])
+ end
 end
---
+++ app/models/purchase_shipping.rb
@@ -0,0 +1,17 @@
+class PurchaseShipping
+  include ActiveModel::Model
+  attr_accessor :postal_code, :prefecture, :city, :house_number, :building_name, :phone_number, :item_id, :user_id
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :phone_number,format: {with: /\A\d{11}\z/, message: "PhoneNumber must be 11 digit Half-width numbers"}
+    validates :selling_price, numericality: { only_integer: true, greater_than: 300, less_than: 9_999_999 }
+  end
+  validates :prefecture, numericality: {other_than: 0, message: "can't be blank"}
+  
+  def save
+   purchase = Parchase.create(item_id: item_id, user_id: user_id)
+   Shipping.create(postal_code: postal_code, prefecture: prefecture, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, purchase_id: purchase.id)
+  end
+end
