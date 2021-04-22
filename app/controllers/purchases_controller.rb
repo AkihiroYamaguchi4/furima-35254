88fc7c45524cb221c23f6def0110584fe8098233
AkihiroYamaguchi4 <81012815+AkihiroYamaguchi4@users.noreply.github.com>
2021-04-22 14:05:07 +0900

    詳細ページの表示

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,6 +1,7 @@
 class PurchasesController < ApplicationController
   before_action :authenticate_user!
   before_action :set_purchase, only: [:index,:create]
+  before_action :move_to_index, only: :index
   def index
    @puchase_shipping = PurchaseShipping.new
   end
@@ -31,5 +32,8 @@ class PurchasesController < ApplicationController
  end
  def set_purchase
   @item = Item.find(params[:item_id])
+ end
+ def move_to_index
+  redirect_to root_path if user_signed_in? && current_user.id == @item.user_id
  end
 end
--- app/models/shipping.rb
+++ app/models/shipping.rb
@@ -2,4 +2,5 @@ class Shipping < ApplicationRecord
   belongs_to :purchase
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :prefecture
+  
 end
